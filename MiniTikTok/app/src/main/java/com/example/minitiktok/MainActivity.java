8d17757d4e65c2155980e82788641a6be02cc826
longxiang-ai <1425507157@qq.com>
2021-07-19 23:47:20 +0800

    添加PlayActivity

--- MiniTikTok/app/src/main/java/com/example/minitiktok/MainActivity.java
+++ MiniTikTok/app/src/main/java/com/example/minitiktok/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.minitiktok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Button btn_play;
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_play = findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PlayActivity.class);
+                Log.d(TAG,"跳转至PlayActivity");
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
