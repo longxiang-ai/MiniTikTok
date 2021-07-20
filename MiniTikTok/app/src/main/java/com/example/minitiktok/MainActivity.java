f4e2826621c15fb33a25d6019cbe59ba802d7c48
longxiang-ai <1425507157@qq.com>
2021-07-20 10:32:04 +0800

    初步实现mainactivity的recyclerview

--- MiniTikTok/app/src/main/java/com/example/minitiktok/MainActivity.java
+++ MiniTikTok/app/src/main/java/com/example/minitiktok/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.minitiktok;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyAdapter.IOnItemClickListener{
 
     protected Button btn_post;
     protected Button btn_search;
@@ -20,20 +26,52 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MyAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private GridLayoutManager gridLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
 
-        btn_post = findViewById(R.id.btn_play);
-        btn_post.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    private void initView() {
+        initButtons();
+        //获取实例
+        recyclerView = findViewById(R.id.recycler);
+        //更改数据时不会变更宽高
+        recyclerView.setHasFixedSize(true);
+        //创建线性布局管理器
+        layoutManager = new LinearLayoutManager(this);
+        //创建格网布局管理器
+        gridLayoutManager = new GridLayoutManager(this, 2);
+        //设置布局管理器
+        recyclerView.setLayoutManager(layoutManager);
+        //创建Adapter
+        mAdapter = new MyAdapter(CoverDataSet.getData());
+        //设置Adapter每个item的点击事件
+        mAdapter.setOnItemClickListener(this);
+        //设置Adapter
+        recyclerView.setAdapter(mAdapter);
+        //分割线
+//        LinearItemDecoration itemDecoration = new LinearItemDecoration(Color.BLUE);
+//        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        //动画
+        DefaultItemAnimator animator = new DefaultItemAnimator();
+        animator.setAddDuration(3000);
+        recyclerView.setItemAnimator(animator);
+    }
+
+    private void initButtons() {
+//        btn_post = findViewById(R.id.btn_play);
+//        btn_post.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
 //                Intent intent = new Intent(MainActivity.this,PostActivity.class);
 //                Log.d(TAG,"跳转至PostActivity");
 //                startActivity(intent);
-            }
-        });
+//            }
+//        });
         btn_search = findViewById(R.id.btn_search);
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,4 +84,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemCLick(int position, CoverData data) {
+        Toast.makeText(MainActivity.this, "点击了第" + position + "条", Toast.LENGTH_SHORT).show();
+        mAdapter.addData(position + 1, new CoverData("新增头条", "0w"));
+    }
+
+    @Override
+    public void onItemLongCLick(int position, CoverData data) {
+        Toast.makeText(MainActivity.this, "长按了第" + position + "条", Toast.LENGTH_SHORT).show();
+//        mAdapter.removeData(position);
+    }
 }
