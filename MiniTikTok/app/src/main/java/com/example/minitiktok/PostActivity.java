2386a6ae57adec2a2ab2fa5ba067d2f1383b3553
longxiang-ai <1425507157@qq.com>
2021-07-22 14:11:42 +0800

    尝试更新upload功能

--- MiniTikTok/app/src/main/java/com/example/minitiktok/PostActivity.java
+++ MiniTikTok/app/src/main/java/com/example/minitiktok/PostActivity.java
@@ -13,6 +13,7 @@ import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -33,12 +34,12 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
         private Camera mCamera;
         private MediaRecorder mMediaRecorder;
         private SurfaceHolder mHolder;
-        private ImageView mImageView;
         private VideoView mVideoView;
         private Button mRecordButton;
+        private Button mUploadButton;
         private boolean isRecording = false;
-
-        private String mp4Path = "";
+        private static String TAG = "PostActivity";
+        private String mp4Path = "123";
 
         public static void startUI(Context context) {
             Intent intent = new Intent(context, PostActivity.class);
@@ -50,13 +51,24 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
             super.onCreate(savedInstanceState);
             setContentView(R.layout.post_activity);
             mSurfaceView = findViewById(R.id.surfaceview);
-            mImageView = findViewById(R.id.iv_img);
+
             mVideoView = findViewById(R.id.videoview);
             mRecordButton = findViewById(R.id.bt_record);
-
+            mUploadButton = findViewById(R.id.bt_upload);
             mHolder = mSurfaceView.getHolder();
             initCamera();
             mHolder.addCallback(this);
+
+            mUploadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "onClick: 尝试点击uploadButton");
+                    Intent intent = new Intent(PostActivity.this,UploadActivity.class);
+                    intent.putExtra("videoPath",mp4Path);
+                    startActivity(intent);
+                }
+            });
+
         }
 
         @Override
@@ -87,7 +99,6 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
-            // todo 3.1 设置 camera 和 holder 建立关联
             try {
                 mCamera.setPreviewDisplay(holder);
                 mCamera.startPreview();
@@ -116,66 +127,24 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
-            // todo 3.2 释放相机
             mCamera.stopPreview();
             mCamera.release();
             mCamera = null;
         }
 
-        public void takePhoto(View view) {
-            mCamera.takePicture(null, null, mPictureCallback);
-        }
-
-        //获取照片中的接口回调
-        Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                // todo 3.3 显示拍照所得图片
-                FileOutputStream fos = null;
-                String filePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator + "1.jpg";
-                File file = new File(filePath);
-                try {
-                    fos = new FileOutputStream(file);
-                    fos.write(data);
-                    fos.flush();
-                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-                    Bitmap rotateBitmap = PathUtils.rotateImage(bitmap,filePath);
-                    mImageView.setVisibility(View.VISIBLE);
-                    mVideoView.setVisibility(View.GONE);
-                    mImageView.setImageBitmap(rotateBitmap);
-                }
-                catch( Exception e)
-                {
-                    e.printStackTrace();
-                }finally {
-                    mCamera.startPreview();
-                    if (fos!=null)
-                    {
-                        try {
-                            fos.close();
-                        }
-                        catch (IOException e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-            }
-        };
 
         public void record(View view) {
             if (!isRecording) {
-                // todo 3.4 开始录制
                 if(prepareVideoRecorder())
                 {
-                    mRecordButton.setText("暂停");
+                    mRecordButton.setText("停止");
                     mMediaRecorder.start();
                 }
             } else {
                 // 停止录制
                 mRecordButton.setText("录制");
-
+                mUploadButton.setVisibility(View.VISIBLE);
+                mRecordButton.setVisibility(View.GONE);
                 mMediaRecorder.setOnErrorListener(null);
                 mMediaRecorder.setOnInfoListener(null);
                 mMediaRecorder.setPreviewDisplay(null);
@@ -184,14 +153,12 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-//            mMediaRecorder.stop();
                 mMediaRecorder.reset();
                 mMediaRecorder.release();
                 mMediaRecorder = null;
                 mCamera.lock();
-                // todo 3.5 播放录制的视频
                 mVideoView.setVisibility(View.VISIBLE);
-                mImageView.setVisibility(View.GONE);
+
                 mVideoView.setVideoPath(mp4Path);
                 mVideoView.start();
             }
@@ -200,26 +167,20 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         private boolean prepareVideoRecorder() {
             mMediaRecorder = new MediaRecorder();
-
             // Step 1: Unlock and set camera to MediaRecorder
             mCamera.unlock();
             mMediaRecorder.setCamera(mCamera);
-
             // Step 2: Set sources
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
             mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-
             // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
             mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
-
             // Step 4: Set output file
             mp4Path = getOutputMediaPath();
             mMediaRecorder.setOutputFile(mp4Path);
-
             // Step 5: Set the preview output
             mMediaRecorder.setPreviewDisplay(mHolder.getSurface());
             mMediaRecorder.setOrientationHint(90);
-
             // Step 6: Prepare configured MediaRecorder
             try {
                 mMediaRecorder.prepare();
@@ -231,7 +192,6 @@ public class PostActivity extends AppCompatActivity implements SurfaceHolder.Cal
         }
 
         private void releaseMediaRecorder() {
-            // todo
             mMediaRecorder.release();
         }
 
---
+++ MiniTikTok/app/src/main/java/com/example/minitiktok/UploadActivity.java
@@ -0,0 +1,200 @@
+package com.example.minitiktok;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.minitiktok.API.UploadResponse;
+import com.example.minitiktok.API.VideoAPI;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.minitiktok.Constants.BASE_URL;
+import static com.example.minitiktok.Constants.STUDENT_ID;
+import static com.example.minitiktok.Constants.USER_NAME;
+import static com.example.minitiktok.Constants.token;
+
+
+public class UploadActivity extends AppCompatActivity {
+    private static final String TAG = "UploadActivity";
+    private static final long MAX_FILE_SIZE = 2 * 1024 * 1024;
+    private static final int REQUEST_CODE_COVER_IMAGE = 101;
+    private static final String COVER_IMAGE_TYPE = "image/*";
+    private static final String VIDEO_TYPE = "video/*";
+    private VideoAPI api;
+    private Uri coverImageUri;
+    private String videoPath;
+//    private Uri videoUri;
+//    private SimpleDraweeView coverSD;
+    private ImageView coverSD;
+    private EditText toEditText;
+    private EditText contentEditText ;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: 开始创建工程");
+
+        initNetwork();
+        setContentView(R.layout.upload_activity);
+        coverSD = findViewById(R.id.sd_cover);
+//        toEditText = findViewById(R.id.et_to);
+//        contentEditText = findViewById(R.id.et_content);
+        findViewById(R.id.btn_cover).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFile(REQUEST_CODE_COVER_IMAGE, COVER_IMAGE_TYPE, "选择图片");
+            }
+        });
+
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submit();
+            }
+        });
+        // ------------------------------ 选择视频路径 -------------------------------
+        Intent intent=getIntent();
+        videoPath =intent.getStringExtra("videoPath");
+        Log.d(TAG, "onCreate: 拿到的videoPath"+videoPath);
+//        videoUri = new Uri(videoUri.parse(videoPath))
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (REQUEST_CODE_COVER_IMAGE == requestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                coverImageUri = data.getData();
+                coverSD.setImageURI(coverImageUri);
+                Log.d(TAG, "onActivityResult: 成功设置????");
+                if (coverImageUri != null) {
+                    Log.d(TAG, "pick cover image " + coverImageUri.toString());
+                } else {
+                    Log.d(TAG, "uri2File fail " + data.getData());
+                }
+
+            } else {
+                Log.d(TAG, "file pick fail");
+            }
+        }
+    }
+
+    private void initNetwork() {
+        final Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        // 生成api对象
+        api = retrofit.create(VideoAPI.class);
+    }
+
+    private void getFile(int requestCode, String type, String title) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(type);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+        intent.putExtra(Intent.EXTRA_TITLE, title);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, requestCode);
+    }
+
+    private void submit() {
+        byte[] coverImageData = readDataFromUri(coverImageUri);
+        if (coverImageData == null || coverImageData.length == 0) {
+            Toast.makeText(this, "封面不存在", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if ( coverImageData.length >= MAX_FILE_SIZE) {
+            Toast.makeText(this, "文件过大", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 封面图选择
+        MultipartBody.Part _coverPart = MultipartBody.Part. createFormData ("image",
+                "cover.png",
+                RequestBody.create(MediaType. parse ("multipart/form-data"),
+                        coverImageData));
+        // 视频信息选择
+        MultipartBody.Part _videoPart = MultipartBody.Part. createFormData ("video",
+                "video.mp4",
+                RequestBody.create(MediaType. parse ("multipart/form-data"),
+                        new File(videoPath)));
+        try{
+            Log.i("upload","尝试上传");
+            Call<UploadResponse> repos = api.submitMessage(STUDENT_ID,USER_NAME,"",_coverPart,_videoPart,token);
+            repos.enqueue(new Callback<UploadResponse>() {
+                @Override
+                public void onResponse(final Call<UploadResponse> call, final Response<UploadResponse> response) {
+                    if (!response.isSuccessful()) {
+                        return;
+                    }
+                    final UploadResponse repoList = response.body();
+                    if (repoList == null) {
+                        return;
+                    }
+                }
+                @Override
+                public void onFailure(final Call<UploadResponse> call, final Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+            Toast.makeText(this,"试一下行不行",Toast.LENGTH_SHORT).show();
+//            finish();
+        }
+        catch (Exception e)
+        {
+            Log.i("upload","尝试上传失败");
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+    private byte[] readDataFromUri(Uri uri) {
+        byte[] data = null;
+        try {
+            InputStream is = getContentResolver().openInputStream(uri);
+            data = Util.inputStream2bytes(is);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+
+}
